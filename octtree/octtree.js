d4e5f706e085f9dc23407528bb62008dc458fc0a
Matthew Sackman <matthew@rabbitmq.com>
2011-06-15 16:15:33 +0100

    Rewrote octtree completely. Much much nicer code now, and far less buggy.

--- octtree/octtree.js
+++ octtree/octtree.js
@@ -1,39 +1,242 @@
-function octtree_root() {
-	var n = Number.MAX_VALUE / 2;
-	return new Octtree(-n, n, -n, n, -n, n);
+var octtree = {};
+octtree.top_nw = 0;
+octtree.top_ne = 1;
+octtree.top_se = 2;
+octtree.top_sw = 3;
+octtree.bot_nw = 4;
+octtree.bot_ne = 5;
+octtree.bot_se = 6;
+octtree.bot_sw = 7;
+octtree.children = [ octtree.top_nw, octtree.top_ne, octtree.top_se,
+		octtree.top_sw, octtree.bot_nw, octtree.bot_ne, octtree.bot_se,
+		octtree.bot_sw ];
+octtree.firstChild = 0;
+octtree.x = 0;
+octtree.y = 1;
+octtree.z = 2;
+
+octtree.findNode = function(tree, pos) {
+	while (true) {
+		if (pos[octtree.x] < tree.xMin || tree.xMax <= pos[octtree.x]
+				|| pos[octtree.y] < tree.yMin || tree.yMax <= pos[octtree.y]
+				|| pos[octtree.z] < tree.zMin || tree.zMax <= pos[octtree.z]) {
+			if (undefined == tree.parent) {
+				return undefined;
+			} else {
+				tree = tree.parent;
+				continue;
+			}
+		}
+
+		if (tree.hasChildren()) {
+			if (pos[octtree.x] < tree.xMid) {
+				if (pos[octtree.y] < tree.yMid) {
+					if (pos[octtree.z] < tree.zMid) {
+						tree = tree[octtree.bot_sw];
+					} else {
+						tree = tree[octtree.bot_nw];
+					}
+				} else {
+					if (pos[octtree.z] < tree.zMid) {
+						tree = tree[octtree.top_sw];
+					} else {
+						tree = tree[octtree.top_nw];
+					}
+				}
+			} else {
+				if (pos[octtree.y] < tree.yMid) {
+					if (pos[octtree.z] < tree.zMid) {
+						tree = tree[octtree.bot_se];
+					} else {
+						tree = tree[octtree.bot_ne];
+					}
+				} else {
+					if (pos[octtree.z] < tree.zMid) {
+						tree = tree[octtree.top_se];
+					} else {
+						tree = tree[octtree.top_ne];
+					}
+				}
+			}
+		} else {
+			return tree;
+		}
+	}
 };
 
-function Octtree(xMin, xMax, yMin, yMax, zMin, zMax, parent) {
-	this.xMin = xMin;
-	this.xMax = xMax;
-	this.yMin = yMin;
-	this.yMax = yMax;
-	this.zMin = zMin;
-	this.zMax = zMax;
-	this.parent = parent;
+octtree.add = function(tree, value) {
+	tree = octtree.findNode(tree, value.pos);
+	if (undefined == tree) {
+		return undefined;
+	} else {
+		var displaced = undefined;
+		while (undefined != value) {
+			if (tree.hasValue()) {
+				if (tree.value.pos[octtree.x] == value.pos[octtree.x]
+						&& tree.value.pos[octtree.y] == value.pos[octtree.y]
+						&& tree.value.pos[octtree.z] == value.pos[octtree.z]) {
+					tree.value = value;
+					value = undefined;
+				} else {
+					displaced = value; // make sure we add our new value last
+					value = tree.value;
+					tree.value = undefined;
 
-	this.xMid = xMin + (xMax - xMin) / 2;
-	this.yMid = yMin + (yMax - yMin) / 2;
-	this.zMid = zMin + (zMax - zMin) / 2;
+					tree[octtree.top_nw] = new Octtree(tree.xMin, tree.xMid,
+							tree.yMid, tree.yMax, tree.zMid, tree.zMax, tree,
+							octtree.top_nw);
+					tree[octtree.top_ne] = new Octtree(tree.xMid, tree.xMax,
+							tree.yMid, tree.yMax, tree.zMid, tree.zMax, tree,
+							octtree.top_ne);
+					tree[octtree.top_se] = new Octtree(tree.xMid, tree.xMax,
+							tree.yMid, tree.yMax, tree.zMin, tree.zMid, tree,
+							octtree.top_se);
+					tree[octtree.top_sw] = new Octtree(tree.xMin, tree.xMid,
+							tree.yMid, tree.yMax, tree.zMin, tree.zMid, tree,
+							octtree.top_sw);
 
-	this.is_empty = is_empty;
-	this.has_value = has_value;
-	this.add = add;
-	this.del = del;
-	this.find = find;
-	this.find4 = find4;
-	this.update = update;
-	this.size = size;
+					tree[octtree.bot_nw] = new Octtree(tree.xMin, tree.xMid,
+							tree.yMin, tree.yMid, tree.zMid, tree.zMax, tree,
+							octtree.bot_nw);
+					tree[octtree.bot_ne] = new Octtree(tree.xMid, tree.xMax,
+							tree.yMin, tree.yMid, tree.zMid, tree.zMax, tree,
+							octtree.bot_ne);
+					tree[octtree.bot_se] = new Octtree(tree.xMid, tree.xMax,
+							tree.yMin, tree.yMid, tree.zMin, tree.zMid, tree,
+							octtree.bot_se);
+					tree[octtree.bot_sw] = new Octtree(tree.xMin, tree.xMid,
+							tree.yMin, tree.yMid, tree.zMin, tree.zMid, tree,
+							octtree.bot_sw);
+					tree = octtree.findNode(tree, value.pos);
+				}
+			} else {
+				tree.value = value;
+				value = displaced;
+				displaced = undefined;
+				if (undefined != value) {
+					tree = octtree.findNode(tree, value.pos);
+				}
+			}
+		}
+		return tree;
+	}
 };
 
-function find(pos, radius, acc) {
-	this.find4(pos, radius, radius * radius, acc);
+octtree.del = function(tree, value) {
+	tree = octtree.findNode(tree, value.pos);
+	if (undefined == tree || (!tree.hasValue())) {
+		return tree;
+	}
+	if (tree.value.pos[octtree.x] == value.pos[octtree.x]
+			&& tree.value.pos[octtree.y] == value.pos[octtree.y]
+			&& tree.value.pos[octtree.z] == value.pos[octtree.z]) {
+		tree.value = undefined;
+		tree = tree.parent;
+		var valCount;
+		var nonEmptyChild = undefined;
+		var child;
+		while (undefined != tree) {
+			valCount = 0;
+			for ( var i = 0; i < octtree.children.length; ++i) {
+				child = octtree.children[i];
+				if (!tree[child].isEmpty()) {
+					valCount++;
+					nonEmptyChild = tree[child];
+				}
+			}
+			if (0 == valCount) {
+				for ( var i = 0; i < octtree.children.length; ++i) {
+					child = octtree.children[i];
+					tree[child] = undefined;
+				}
+				tree = tree.parent;
+			} else if (1 == valCount) {
+				if (nonEmptyChild.hasValue()) {
+					for ( var i = 0; i < octtree.children.length; ++i) {
+						child = octtree.children[i];
+						tree[child] = undefined;
+					}
+					tree.value = nonEmptyChild.value;
+					tree = tree.parent;
+				} else {
+					break;
+				}
+			} else {
+				break;
+			}
+		}
+	}
+	return tree;
 };
 
-function find4(pos, radius, radiusSq, acc) {
-	var worklist = new Array(this);
-	var tree = undefined;
+octtree.next = function(tree) {
+	while (undefined != tree) {
+		var nextChildId = (tree.childId + 1) % octtree.children.length;
+		if (nextChildId > tree.childId && undefined != tree.parent) {
+			return tree.parent[nextChildId];
+		} else {
+			tree = tree.parent;
+		}
+	}
+	return undefined;
+};
 
+octtree.defined = function(a, b) {
+	if (undefined == a) {
+		return b;
+	} else {
+		return a;
+	}
+};
+
+octtree.update = function(tree) {
+	var root = tree;
+	var parent = root.parent;
+	root.parent = undefined; // do this to stop next going up past tree
+
+	var v = undefined;
+	var movedValues = new Array();
+	while (undefined != tree) {
+		if (tree.hasValue()) {
+			v = tree.value;
+			if (v.next_pos[octtree.x] < tree.xMin
+					|| tree.xMax <= v.next_pos[octtree.x]
+					|| v.next_pos[octtree.y] < tree.yMin
+					|| tree.yMax <= v.next_pos[octtree.y]
+					|| v.next_pos[octtree.z] < tree.zMin
+					|| tree.zMax <= v.next_pos[octtree.z]) {
+				movedValues.push(tree.value);
+			} else {
+				vec3.set(v.next_pos, v.pos);
+			}
+			tree = octtree.next(tree);
+		} else if (tree.hasChildren()) {
+			tree = tree[octtree.firstChild];
+		} else {
+			tree = octtree.next(tree);
+		}
+	}
+
+	root.parent = parent;
+	tree = root;
+	for ( var i = 0; i < movedValues.length; ++i) {
+		v = movedValues[i];
+		tree = octtree.defined(tree.del(v), tree);
+		//root.del(v);
+		vec3.set(v.next_pos, v.pos);
+		//root.add(v);
+		tree = octtree.defined(tree.add(v), tree);
+	}
+
+	return root;
+};
+
+octtree.findInRadius = function(tree, pos, radius, limit) {
+	var acc = new Array();
+	var radiusSq = radius * radius;
+	var worklist = new Array(tree);
+	tree = undefined;
+	
 	var x_p_r = 0;
 	var x_m_r = 0;
 	var y_p_r = 0;
@@ -41,17 +244,17 @@ function find4(pos, radius, radiusSq, acc) {
 	var z_p_r = 0;
 	var z_m_r = 0;
 
-	while (0 < worklist.length) {
+	while (0 < worklist.length && (undefined == limit || limit > acc.length)) {
 		tree = worklist.shift();
 
-		if (tree.is_empty()) {
+		if (tree.isEmpty()) {
 			continue;
 		}
 
-		if (tree.has_value()) {
-			var xd = Math.abs(tree.value.pos[0] - pos[0]);
-			var yd = Math.abs(tree.value.pos[1] - pos[1]);
-			var zd = Math.abs(tree.value.pos[2] - pos[2]);
+		if (tree.hasValue()) {
+			var xd = Math.abs(tree.value.pos[octtree.x] - pos[octtree.x]);
+			var yd = Math.abs(tree.value.pos[octtree.y] - pos[octtree.y]);
+			var zd = Math.abs(tree.value.pos[octtree.z] - pos[octtree.z]);
 			xd *= xd;
 			yd *= yd;
 			zd *= zd;
@@ -61,12 +264,12 @@ function find4(pos, radius, radiusSq, acc) {
 			continue;
 		}
 
-		x_p_r = pos[0] + radius;
-		x_m_r = pos[0] - radius;
-		y_p_r = pos[1] + radius;
-		y_m_r = pos[1] - radius;
-		z_p_r = pos[2] + radius;
-		z_m_r = pos[2] - radius;
+		x_p_r = pos[octtree.x] + radius;
+		x_m_r = pos[octtree.x] - radius;
+		y_p_r = pos[octtree.y] + radius;
+		y_m_r = pos[octtree.y] - radius;
+		z_p_r = pos[octtree.z] + radius;
+		z_m_r = pos[octtree.z] - radius;
 
 		if (x_p_r < tree.xMin || tree.xMax <= x_m_r || y_p_r < tree.yMin
 				|| tree.yMax <= y_m_r || z_p_r < tree.zMin
@@ -77,291 +280,110 @@ function find4(pos, radius, radiusSq, acc) {
 		if (x_m_r < tree.xMid) {
 			if (y_m_r < tree.yMid) {
 				if (z_m_r < tree.zMid) {
-					worklist.push(tree.bot_sw);
+					worklist.push(tree[octtree.bot_sw]);
 				}
 				if (tree.zMid <= z_p_r) {
-					worklist.push(tree.bot_nw);
+					worklist.push(tree[octtree.bot_nw]);
 				}
 			}
 			if (tree.yMid <= y_p_r) {
 				if (z_m_r < tree.zMid) {
-					worklist.push(tree.top_sw);
+					worklist.push(tree[octtree.top_sw]);
 				}
 				if (tree.zMid <= z_p_r) {
-					worklist.push(tree.top_nw);
+					worklist.push(tree[octtree.top_nw]);
 				}
 			}
 		}
 		if (tree.xMid <= x_p_r) {
 			if (y_m_r < tree.yMid) {
 				if (z_m_r < tree.zMid) {
-					worklist.push(tree.bot_se);
+					worklist.push(tree[octtree.bot_se]);
 				}
 				if (tree.zMid <= z_p_r) {
-					worklist.push(tree.bot_ne);
+					worklist.push(tree[octtree.bot_ne]);
 				}
 			}
 			if (tree.yMid <= y_p_r) {
 				if (z_m_r < tree.zMid) {
-					worklist.push(tree.top_se);
+					worklist.push(tree[octtree.top_se]);
 				}
 				if (tree.zMid <= z_p_r) {
-					worklist.push(tree.top_ne);
+					worklist.push(tree[octtree.top_ne]);
 				}
 			}
 		}
 	}
+	return acc;
 };
 
-function add(e) {
-	var worklist = new Array();
-	var v = e;
-	var tree = this;
-
-	while ((!(tree == undefined)) || worklist.length > 0) {
-		if (tree == undefined) {
-			v = worklist.shift();
-			tree = worklist.shift();
-		}
-
-		if (v.pos[0] < tree.xMin || tree.xMax <= v.pos[0]
-				|| v.pos[1] < tree.yMin || tree.yMax <= v.pos[1]
-				|| v.pos[2] < tree.zMin || tree.zMax <= v.pos[2]) {
-			// if the value is outside the root, then it'll just get silently
-			// ignored. This is "safe".
-			tree = tree.parent;
-			continue;
-		}
-
-		if (tree.is_empty()
-				|| (tree.has_value() && v.pos[0] == tree.value.pos[0]
-						&& v.pos[1] == tree.value.pos[1] && v.pos[2] == tree.value.pos[2])) {
-			tree.value = v;
-			tree = undefined;
-			continue;
-		}
-
-		if (tree.has_value()) {
-			tree.top_nw = new Octtree(tree.xMin, tree.xMid, tree.yMid,
-					tree.yMax, tree.zMid, tree.zMax, tree);
-			tree.top_ne = new Octtree(tree.xMid, tree.xMax, tree.yMid,
-					tree.yMax, tree.zMid, tree.zMax, tree);
-			tree.top_se = new Octtree(tree.xMid, tree.xMax, tree.yMid,
-					tree.yMax, tree.zMin, tree.zMid, tree);
-			tree.top_sw = new Octtree(tree.xMin, tree.xMid, tree.yMid,
-					tree.yMax, tree.zMin, tree.zMid, tree);
-
-			tree.bot_nw = new Octtree(tree.xMin, tree.xMid, tree.yMin,
-					tree.yMid, tree.zMid, tree.zMax, tree);
-			tree.bot_ne = new Octtree(tree.xMid, tree.xMax, tree.yMin,
-					tree.yMid, tree.zMid, tree.zMax, tree);
-			tree.bot_se = new Octtree(tree.xMid, tree.xMax, tree.yMin,
-					tree.yMid, tree.zMin, tree.zMid, tree);
-			tree.bot_sw = new Octtree(tree.xMin, tree.xMid, tree.yMin,
-					tree.yMid, tree.zMin, tree.zMid, tree);
-
-			worklist.push(tree.value);
-			worklist.push(tree);
-			tree.value = undefined;
-		}
-
-		if (v.pos[0] < tree.xMid) {
-			if (v.pos[1] < tree.yMid) {
-				if (v.pos[2] < tree.zMid) {
-					tree = tree.bot_sw;
-				} else {
-					tree = tree.bot_nw;
-				}
-			} else {
-				if (v.pos[2] < tree.zMid) {
-					tree = tree.top_sw;
-				} else {
-					tree = tree.top_nw;
-				}
-			}
-		} else {
-			if (v.pos[1] < tree.yMid) {
-				if (v.pos[2] < tree.zMid) {
-					tree = tree.bot_se;
-				} else {
-					tree = tree.bot_ne;
-				}
-			} else {
-				if (v.pos[2] < tree.zMid) {
-					tree = tree.top_se;
-				} else {
-					tree = tree.top_ne;
-				}
-			}
-		}
-	}
-};
-
-function del(e) {
-	var v = e;
-	var tree = this;
-
-	while (!(undefined == tree)) {
-		if (tree.has_value()) {
-			if (v.pos[0] == tree.value.pos[0] && v.pos[1] == tree.value.pos[1]
-					&& v.pos[2] == tree.value.pos[2]) {
-				tree.value = undefined;
-			}
-			break;
-		} else if (tree.is_empty()) {
-			break;
-		} else {
-			if (v.pos[0] < tree.xMin || tree.xMax <= v.pos[0]
-					|| v.pos[1] < tree.yMin || tree.yMax <= v.pos[1]
-					|| v.pos[2] < tree.zMin || tree.zMax <= v.pos[2]) {
-				tree = tree.parent;
-			} else {
-				if (v.pos[0] < tree.xMid) {
-					if (v.pos[1] < tree.yMid) {
-						if (v.pos[2] < tree.zMid) {
-							tree = tree.bot_sw;
-						} else {
-							tree = tree.bot_nw;
-						}
-					} else {
-						if (v.pos[2] < tree.zMid) {
-							tree = tree.top_sw;
-						} else {
-							tree = tree.top_nw;
-						}
-					}
-				} else {
-					if (v.pos[1] < tree.yMid) {
-						if (v.pos[2] < tree.zMid) {
-							tree = tree.bot_se;
-						} else {
-							tree = tree.bot_ne;
-						}
-					} else {
-						if (v.pos[2] < tree.zMid) {
-							tree = tree.top_se;
-						} else {
-							tree = tree.top_ne;
-						}
-					}
-				}
-			}
-		}
-	}
-};
-
-function has_value() {
-	return (!(this.value == undefined));
-};
-
-function is_empty() {
-	return (this.value == undefined && this.top_nw == undefined);
-};
-
-function update() {
-	var tree = this;
-	var worklist = new Array();
-	var v = undefined;
-	var pruneable = new Array();
-
-	while (!(undefined == tree)) {
-		if (tree.has_value()) {
-			v = tree.value;
-			if (v.next_pos[0] < tree.xMin || tree.xMax <= v.next_pos[0]
-					|| v.next_pos[1] < tree.yMin || tree.yMax <= v.next_pos[1]
-					|| v.next_pos[2] < tree.zMin || tree.zMax <= v.next_pos[2]) {
-				tree.del(v);
-				vec3.set(v.next_pos, v.pos);
-				this.add(v);
-				if (!(undefined == tree.parent)) {
-					pruneable.push(tree.parent);
-				}
-			} else {
-				vec3.set(v.next_pos, v.pos);
-			}
-		} else if (!tree.is_empty()) {
-			worklist.push(tree.top_nw);
-			worklist.push(tree.top_ne);
-			worklist.push(tree.top_sw);
-			worklist.push(tree.top_se);
-			worklist.push(tree.bot_nw);
-			worklist.push(tree.bot_ne);
-			worklist.push(tree.bot_sw);
-			worklist.push(tree.bot_se);
-		}
-		tree = worklist.shift();
-	}
-	prune(pruneable);
-};
-
-function size() {
+octtree.size = function(tree) {
 	var count = 0;
-	var tree = this;
-	var worklist = new Array();
-	while (!(undefined == tree)) {
-		if (tree.has_value()) {
+	var root = tree;
+	var parent = root.parent;
+	root.parent = undefined; // stop the traversal going above us.
+
+	while (undefined != tree) {
+		if (tree.hasValue()) {
 			count++;
-		} else if (!tree.is_empty()) {
-			worklist.push(tree.top_nw);
-			worklist.push(tree.top_ne);
-			worklist.push(tree.top_sw);
-			worklist.push(tree.top_se);
-			worklist.push(tree.bot_nw);
-			worklist.push(tree.bot_ne);
-			worklist.push(tree.bot_sw);
-			worklist.push(tree.bot_se);
+			tree = octtree.next(tree);
+		} else if (tree.hasChildren()) {
+			tree = tree[octtree.firstChild];
+		} else {
+			tree = octtree.next(tree);
 		}
-		tree = worklist.shift();
 	}
+
+	root.parent = parent;
 	return count;
+};
+
+octtree.create = function(xMin, xMax, yMin, yMax, zMin, zMax) {
+	return new Octtree(xMin, xMax, yMin, yMax, zMin, zMax, undefined, undefined);
+};
+
+function Octtree(xMin, xMax, yMin, yMax, zMin, zMax, parent, childId) {
+	this.xMin = xMin;
+	this.xMax = xMax;
+	this.yMin = yMin;
+	this.yMax = yMax;
+	this.zMin = zMin;
+	this.zMax = zMax;
+	this.parent = parent;
+	this.childId = childId;
+
+	this.xMid = xMin + (xMax - xMin) / 2;
+	this.yMid = yMin + (yMax - yMin) / 2;
+	this.zMid = zMin + (zMax - zMin) / 2;
+
+	this.isEmpty = isEmpty;
+	this.hasChildren = hasChildren;
+	this.hasValue = hasValue;
+	this.add = function(value) {
+		return octtree.add(this, value);
+	};
+	this.del = function(value) {
+		return octtree.del(this, value);
+	};
+	this.update = function() {
+		return octtree.update(this);
+	};
+	this.findInRadius = function(pos, radius, limit) {
+		return octtree.findInRadius(this, pos, radius, limit);
+	};
+	this.size = function() {
+		return octtree.size(this);
+	};
+};
+
+function isEmpty() {
+	return (undefined == this[octtree.firstChild]) && (undefined == this.value);
 }
 
-function prune(worklist) {
-	var tree = worklist.shift();
-	while (!(undefined == tree)) {
-		if (tree.has_value()) {
+function hasChildren() {
+	return undefined != this[octtree.firstChild];
+}
 
-		} else if (!tree.is_empty()) {
-			if (tree.top_sw.is_empty() && tree.top_se.is_empty()
-					&& tree.top_nw.is_empty() && tree.top_ne.is_empty()
-					&& tree.bot_sw.is_empty() && tree.bot_se.is_empty()
-					&& tree.bot_nw.is_empty() && tree.bot_ne.is_empty()) {
-				tree.top_sw = undefined;
-				tree.top_se = undefined;
-				tree.top_nw = undefined;
-				tree.top_ne = undefined;
-				tree.bot_sw = undefined;
-				tree.bot_se = undefined;
-				tree.bot_nw = undefined;
-				tree.bot_ne = undefined;
-				worklist.push(tree.parent);
-			} else {
-				if (!tree.top_sw.is_empty()) {
-					worklist.push(tree.top_sw);
-				}
-				if (!tree.top_se.is_empty()) {
-					worklist.push(tree.top_se);
-				}
-				if (!tree.top_nw.is_empty()) {
-					worklist.push(tree.top_nw);
-				}
-				if (!tree.top_ne.is_empty()) {
-					worklist.push(tree.top_ne);
-				}
-				if (!tree.bot_sw.is_empty()) {
-					worklist.push(tree.bot_sw);
-				}
-				if (!tree.bot_se.is_empty()) {
-					worklist.push(tree.bot_se);
-				}
-				if (!tree.bot_nw.is_empty()) {
-					worklist.push(tree.bot_nw);
-				}
-				if (!tree.bot_ne.is_empty()) {
-					worklist.push(tree.bot_ne);
-				}
-			}
-		}
-		tree = worklist.shift();
-	}
+function hasValue() {
+	return undefined != this.value;
 }
