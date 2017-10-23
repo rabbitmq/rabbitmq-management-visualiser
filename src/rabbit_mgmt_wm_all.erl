fa6ad61d7c59674a1bd65d28238f7a1de75c88be
Daniil Fedotov <dfedotov@pivotal.io>
2017-10-23 11:22:59 +0100

    Support cowboy2

--- src/rabbit_mgmt_wm_all.erl
+++ src/rabbit_mgmt_wm_all.erl
@@ -14,8 +14,8 @@
 %%   Copyright (c) 2007-2017 Pivotal Software, Inc.  All rights reserved.
 -module(rabbit_mgmt_wm_all).
 
--export([init/3]).
--export([rest_init/2, to_json/2, content_types_provided/2, is_authorized/2,
+-export([init/2]).
+-export([to_json/2, content_types_provided/2, is_authorized/2,
          resource_exists/2]).
 
 -import(rabbit_misc, [pget/2]).
@@ -24,9 +24,8 @@
 -include_lib("amqp_client/include/amqp_client.hrl").
 
 %%--------------------------------------------------------------------
-init(_, _, _) -> {upgrade, protocol, cowboy_rest}.
-
-rest_init(ReqData, _) -> {ok, ReqData, #context{}}.
+init(Req, _State) ->
+    {cowboy_rest, rabbit_mgmt_cors:set_headers(Req, ?MODULE), #context{}}.
 
 content_types_provided(ReqData, Context) ->
    {[{<<"application/json">>, to_json}], ReqData, Context}.
